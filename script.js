e8074543b7ccc499d90f63e3b6e1664dbee5a15a
Elliot Bohlen <bohle000@newschool.edu>
2020-04-13 18:25:18 -0400

    pos

--- script.js
+++ script.js
@@ -19,15 +19,15 @@ var append1Function = setInterval(function(){
   //inserting
  var pic1 = `<div class="patternunit" style="
    left:${i * 40}px;
-   top:${i * 50}px;
-   transform: scale(${(i+4)/10}); opacity:${n};
+   top:${i * 40}px;
+   transform: scale(${(i)/10}); opacity:${n};
    "><img src="images/edited/Largemirror(candeliver).png"></div>`;
  document.getElementById("box1").insertAdjacentHTML("beforeend", pic1);
 
  var pic2 = `<div class="patternunit" style="
    right:${i * 40}px;
    top:${i * 20}px;
-   transform: scale(${(i+4)/10}); opacity:${n};
+   transform: scale(${(i)/10}); opacity:${n};
 
    "><img src="images/edited/vintageWalnutframed.png"></div>`;
  document.getElementById("box2").insertAdjacentHTML("beforeend", pic2);
@@ -35,28 +35,28 @@ var append1Function = setInterval(function(){
  var pic3 = `<div class="patternunit" style="
    right:${i * 20}px;
    top:${i * 20}px;
-   transform: scale(${(i+4)/10}); opacity:${n};
+   transform: scale(${(i)/10}); opacity:${n};
    "><img src="images/edited/verysolid.png"></div>`;
  document.getElementById("box3").insertAdjacentHTML("beforeend", pic3);
 
  var pic4 = `<div class="patternunit" style="
    right:${i * 20}px;
-   top:${i * 60}px;
-   transform: scale(${(i+4)/10}); opacity:${n};
+   top:${i * 50}px;
+   transform: scale(${(i)/10}); opacity:${n};
    "><img src="images/edited/ovaltopcopy.png"></div>`;
  document.getElementById("box4").insertAdjacentHTML("beforeend", pic4);
 
  var pic5 = `<div class="patternunit" style="
    right:${i * 10}px;
    top:${i * 20}px;
-   transform: scale(${(i+4)/10}); opacity:${n};
+   transform: scale(${(i)/10}); opacity:${n};
    "><img src="images/edited/AntiqueMirror.png"></div>`;
  document.getElementById("box5").insertAdjacentHTML("beforeend", pic5);
 
  var pic6 = `<div class="patternunit" style="
-   right:${i * 40}px;
-   top:${i * 60}px;
-   transform: scale(${(i+4)/10}); opacity:${n};
+   right:${i * 50}px;
+   top:${i * 70}px;
+   transform: scale(${(i)/10}); opacity:${n};
 
    "><img src="images/edited/VictorianVintage.png"></div>`;
  document.getElementById("box6").insertAdjacentHTML("beforeend", pic6);
@@ -64,21 +64,21 @@ var append1Function = setInterval(function(){
  var pic7 = `<div class="patternunit" style="
    left:${i * 20}px;
    top:${i * 20}px;
-   transform: scale(${(i+4)/10}); opacity:${n};
+   transform: scale(${(i)/10}); opacity:${n};
    "><img src="images/edited/sunrayscopy.png"></div>`;
  document.getElementById("box7").insertAdjacentHTML("beforeend", pic7);
 
  var pic8 = `<div class="patternunit" style="
    right:${i * 20}px;
    top:${i * 20}px;
-   transform: scale(${(i+4)/10}); opacity:${n};
+   transform: scale(${(i)/10}); opacity:${n};
    "><img src="images/edited/antiquehandcarved.png"></div>`;
  document.getElementById("box8").insertAdjacentHTML("beforeend", pic8);
 
  var pic9 = `<div class="patternunit" style="
-   right:${i * 40}px;
+   right:${i * 50}px;
    top:${i*10}px;
-   transform: scale(${(i+4)/10}); opacity:${n};
+   transform: scale(${(i)/10}); opacity:${n};
    "><img src="images/edited/large.png"></div>`;
  document.getElementById("box9").insertAdjacentHTML("beforeend", pic9);
 
@@ -91,30 +91,30 @@ var append1Function = setInterval(function(){
 
 
  var pic10 = `<div class="patternunit" style="
-   left:${i * 90}px;
-   top:${i * 20}px;
-   transform: scale(${(i+4)/10}); opacity:${n};
+   left:${i * 60}px;
+   bottom:${i * 20}px;
+   transform: scale(${(i)/10}); opacity:${n};
    "><img src="images/edited/DoubleSidedRollingcopy.png"></div>`;
  document.getElementById("box10").insertAdjacentHTML("beforeend", pic10);
 
  var pic11 = `<div class="patternunit" style="
-   right:${i * 90}px;
+   right:${i * 30}px;
    top:${i * 20}px;
-   transform: scale(${(i+4)/10}); opacity:${n};
+   transform: scale(${(i)/10}); opacity:${n};
    "><img src="images/edited/AntiqueMirror150.png"></div>`;
  document.getElementById("box11").insertAdjacentHTML("beforeend", pic11);
 
  var pic12 = `<div class="patternunit" style="
    right:${i * 10}px;
    top:${i * 20}px;
-   transform: scale(${(i+4)/10}); opacity:${n};
+   transform: scale(${(i)/10}); opacity:${n};
    "><img src="images/edited/Antiquedresserwithmirror.png"></div>`;
  document.getElementById("box12").insertAdjacentHTML("beforeend", pic12);
 
  var pic13 = `<div class="patternunit" style="
    left:${i * 20}px;
    top:${i * 30}px;
-   transform: scale(${(i+4)/10}); opacity:${n};
+   transform: scale(${(i)/10}); opacity:${n};
    "><img src="images/edited/ScreenShot.png
 "></div>`;
  document.getElementById("box13").insertAdjacentHTML("beforeend", pic13);
@@ -123,7 +123,7 @@ var append1Function = setInterval(function(){
    left:${i * 90}px;
    bottom:${i * 20}px;
    display:none;
-   transform: scale(${(i+4)/10}); opacity:${n};
+   transform: scale(${(i)/10}); opacity:${n};
    "><img src="images/edited/retrolookcopy.png
 "></div>`;
  document.getElementById("box14").insertAdjacentHTML("beforeend", pic14);
@@ -131,7 +131,7 @@ var append1Function = setInterval(function(){
  var pic15 = `<div class="patternunit" style="
    right:${i * 10}px;
    bottom:${i * 20}px;
-   transform: scale(${(i+4)/10}); opacity:${n};
+   transform: scale(${(i)/10}); opacity:${n};
    "><img src="images/edited/AntiqueVanitywithMirror.png
 "></div>`;
  document.getElementById("box15").insertAdjacentHTML("beforeend", pic15);
@@ -143,7 +143,7 @@ var append1Function = setInterval(function(){
  }
  i++;
  n += .067;
-},1000); //speed in ms
+},500); //speed in ms
 
 
 $( function() {
